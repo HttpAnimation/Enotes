8bdddfcdcd31a4a5745ffe37f677b27d0a8f9b53
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-31 18:21:23 -0700

    auto save

--- Enotes/renderer.js
+++ Enotes/renderer.js
@@ -3,15 +3,20 @@ const { ipcRenderer } = require('electron');
 
 const noteTextarea = document.getElementById('note');
 
+// Function to save note to file
+function saveNoteToFile(content) {
+  fs.writeFile('Enotes/note.txt', content, (err) => {
+    if (err) throw err;
+  });
+}
+
 // Load note from file
-fs.readFile('note.txt', 'utf8', (err, data) => {
+fs.readFile('Enotes/note.txt', 'utf8', (err, data) => {
   if (err) return;
   noteTextarea.value = data;
 });
 
-// Save note to file
+// Save note to file whenever there is a change in the textarea content
 noteTextarea.addEventListener('input', () => {
-  fs.writeFile('note.txt', noteTextarea.value, (err) => {
-    if (err) throw err;
-  });
+  saveNoteToFile(noteTextarea.value);
 });
